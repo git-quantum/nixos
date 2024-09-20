6302a950787499fcb24be1734ced0112360a892d
git-quantum <quentinhr@protonmail.com>
2024-09-20 16:38:08 +0200

    Setup module

--- nixos-flake/modules/home-manager/components/gaming/default.nix
+++ nixos-flake/modules/home-manager/components/gaming/default.nix
@@ -1,3 +1,46 @@
+{ config, pkgs, ... }:
+
+# Ensure gs.sh is created if gamingMode on
+let
+  # Check if Steam is installed on the system
+  steamInstalled = builtins.exec "which steam" == 0;
+
+  gsScript = ''
+        #!/usr/bin/env bash
+                set -xeuo pipefail
+
+                gamescopeArgs=(
+                    --adaptive-sync # VRR support
+                    --hdr-enabled
+                    --mangoapp # performance overlay
+                    --rt
+                    --steam
+                )
+                steamArgs=(
+                    -pipewire-dmabuf
+                    -tenfoot
+                )
+                mangoConfig=(
+                    cpu_temp
+                    gpu_temp
+                    ram
+                    vram
+                )
+                mangoVars=(
+                    MANGOHUD=1
+                    MANGOHUD_CONFIG="$(IFS=,; echo "${mangoConfig[*]}")"
+                )
+
+                export "${mangoVars[@]}"
+                exec gamescope "${gamescopeArgs[@]}" -- steam "${steamArgs[@]}"
+    ''
+in
 {
-    
+  # Create the .sh if steam is installed
+  home = {
+    file.".config/hm-modules/gaming/gs.sh" = {
+        text = if steamInstalled then gsScript;
+        executable = true; chmod +x the the new script
+    };
+  };
 }
