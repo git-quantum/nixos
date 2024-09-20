3b65e7923def93c32ebb5d0f8bd4e5454fa1df90
git-quantum <quentinhr@protonmail.com>
2024-09-20 14:43:05 +0200

    Update module for home-manager

--- nixos-flake/modules/home-manager/components/zsh.nix
+++ nixos-flake/modules/home-manager/components/zsh.nix
@@ -10,16 +10,16 @@ in {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestions.enable = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     shellAliases = customAliases;
-    loginShellInit = "freshfetch";
     dotDir = ".config/zsh";
-    promptInit = ''
+    initExtra = ''
       PROMPT=" ◉ %U%F{magenta}%n%f%u@%U%F{blue}%m%f%u:%F{yellow}%~%f
       %F{green}→%f "
       RPROMPT="%F{red}▂%f%F{yellow}▄%f%F{green}▆%f%F{cyan}█%f%F{blue}▆%f%F{magenta}▄%f%F{white}▂%f"
       [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
+      freshfetch
     '';
 
   };         
