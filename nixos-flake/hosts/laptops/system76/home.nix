8e205ed18b9da0e253f812f735b91523efb216d8
git-quantum <quentinhr@protonmail.com>
2024-09-21 12:15:57 +0200

    test: Update import list

--- nixos-flake/hosts/laptops/system76/home.nix
+++ nixos-flake/hosts/laptops/system76/home.nix
@@ -4,12 +4,16 @@
 
   imports = [
     ../../../modules/home-manager
+    ../../../modules/home-manager/scripts/check_bin.nix
   ];
+  
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "quantum";
   home.homeDirectory = "/home/quantum";
 
+
+  pkgChecker.pkgName = "steam";
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
