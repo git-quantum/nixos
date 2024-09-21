efedc12168d57b39707d1631ccd7e081c6ca35b6
git-quantum <quentinhr@protonmail.com>
2024-09-21 12:16:16 +0200

    Update module

--- nixos-flake/modules/home-manager/components/common/helix.nix
+++ nixos-flake/modules/home-manager/components/common/helix.nix
@@ -1,49 +1,53 @@
+{ pkgs, ... }:
 {
-  programs.helix.enable = true;
+  programs.helix = {
+    enable = true;
   
-  programs.helix.settings = {
-    theme = "catppuccin_mocha";
-    editor = {
-      line-number = "relative";
-      mouse = true;
-      middle-click-paste = true;
-      auto-completion = true;
-      color-modes = true;
-      bufferline = "multiple";
-      cursorline = true;
-      rulers = [120];
-      true-color = true;
-      #end-of-line-diagnostics = "hint";
-      
-      #inline-diagnostics = {
-      #  cursor-line = "warning"; # show warnings and errors on the cursorline inline
-      #};
-      
-      cursor-shape = {
-        insert = "bar";
-        normal = "block";
-        select = "underline";
-      };
-      
-      file-picker = {
-        hidden = false;
-      };
+    settings = {
+      theme = "catppuccin_mocha";
+      editor = {
+        line-number = "relative";
+        mouse = true;
+        middle-click-paste = true;
+        auto-completion = true;
+        color-modes = true;
+        bufferline = "multiple";
+        cursorline = true;
+        rulers = [120];
+        true-color = true;
+        
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        
+        file-picker = {
+          hidden = false;
+        };
 
-      indent-guides = {
-        character = "╎";
-        render = true;
-      };
+        indent-guides = {
+          character = "╎";
+          render = true;
+        };
 
-      lsp = {
-        # Disable automatically popups of signature parameter help
-        auto-signature-help = false;
-        # Show LSP messages in the status line
-        display-messages = true;
-      };
+        lsp = {
+          # Disable automatically popups of signature parameter help
+          auto-signature-help = false;
+          # Show LSP messages in the status line
+          display-messages = true;
+        };
 
-      statusline = {
-        left = [ "mode" "spinner" "version-control" "file-name" ];
+        statusline = {
+          left = [ "mode" "spinner" "version-control" "file-name" ];
+        };
       };
     };  
+  
+    languages.language = [{
+      name = "nix";
+      auto-format = true;
+      formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+    }];
   };
 }
