6ae3ea71a5c6813ec5989e51061efafae891d887
git-quantum <quentinhr@protonmail.com>
2024-09-21 03:50:27 +0200

    Update module

--- nixos-flake/modules/nixos/components/gpu.nix
+++ nixos-flake/modules/nixos/components/gpu.nix
@@ -3,7 +3,7 @@
   options = {
     custom.gpu.mode = lib.mkOption {
       type = lib.types.enum[ "amd" "nvidia" ];
-      description = " Choose whether to use 'amd' or 'nvidia' for GPU setup.";
+      description = "Choose whether to use 'amd' or 'nvidia' for GPU setup.";
       default = "amd";
     };
   };
