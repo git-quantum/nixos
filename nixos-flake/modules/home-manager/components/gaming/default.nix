0846570fe8b085beed1bf4ba484ef3857eed7175
git-quantum <quentinhr@protonmail.com>
2024-09-21 13:25:44 +0200

    Update module

--- nixos-flake/modules/home-manager/components/gaming/default.nix
+++ nixos-flake/modules/home-manager/components/gaming/default.nix
@@ -7,16 +7,16 @@ let
     };
 
     # Check if Steam is installed on system
-    isSteamInstalled = lib.mkIf (lib.elem pkgs.steam (config.environment.systemPackages or []));
+    isSteamInstalled = lib.elem pkgs.steam (config.environment.systemPackages or []);
    
 in
 {
     # Copy script in target locations if Steam installed
-    home.file = isSteamInstalled {
-        "gs.sh" = {
+    home.file = {
+        "gs.sh" = if isSteamInstalled then {
             source = gsScriptPath;
             target = ".config/hm-modules/gaming/gs.sh";
             executable = true;
-        };
+        } else null;
     };
 }
