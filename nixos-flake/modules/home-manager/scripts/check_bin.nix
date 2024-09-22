0ca3d8e6fea1b4202f7b858659487f378dfdff2d
git-quantum <quentinhr@protonmail.com>
2024-09-22 03:33:51 +0200

    Delete draft module

--- nixos-flake/modules/home-manager/scripts/check_bin.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs }:
-pkgs.writeShellScriptBin "pkgChecker" ''
-  if test -x /run/current-system/sw/bin/$1;
-  then exit 0
-  else exit 1
-''
--- nixos-flake/modules/home-manager/scripts/check_bin.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-if test -x /run/current-system/sw/bin/$1;
-  then exit 0
-  else exit 1
