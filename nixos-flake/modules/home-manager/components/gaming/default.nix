b52d85f849866b46591584ffce6b624da382ffa2
git-quantum <quentinhr@protonmail.com>
2024-09-22 03:34:41 +0200

    Fix module

--- nixos-flake/modules/home-manager/components/gaming/default.nix
+++ nixos-flake/modules/home-manager/components/gaming/default.nix
@@ -1,22 +1,35 @@
 { config, pkgs, lib, ... }:
-let 
-    # Add script's location to Nixos managed paths
-    gsScriptPath = builtins.path {
-      name = "gs.sh";
-      path = ./../../scripts/gs.sh;
-    };
 
-    # Check if Steam is installed on system
-    isSteamInstalled = lib.mkIf (builtins.elem pkgs.steam (config.environment.systemPackages or []));
-   
-in
-{
-    # Copy script in target locations if Steam installed
-    home.file = isSteamInstalled {
-        "gs.sh" = {
-            source = gsScriptPath;
-            target = ".config/hm-modules/gaming/gs.sh";
-            executable = true;
-        };
+let
+  # Add script's location to Nixos managed paths
+  gsScriptPath = builtins.path {
+    name = "gs.sh";
+    path = ./../../scripts/gs.sh;
+  };
+
+  # Get pkgs at system and user level
+  systemPackages = config.environment.systemPackages or [ ];
+  userPackages = config.home.packages or [ ];
+
+  # Check if pkgs.steam is present in user packages
+  isSteamInstalled = lib.elem pkgs.steam userPackages;
+
+in {
+  # Debug
+  home.activation = {
+    steamCheck = ''
+      echo "System packages: ${toString systemPackages}"
+      echo "User packages: ${toString userPackages}"
+      echo "Steam installed: ${toString isSteamInstalled}"
+    '';
+  };
+
+  # Add dotfile if steam installed
+  home.file = lib.mkIf isSteamInstalled {
+    "gs.sh" = {
+      source = gsScriptPath;
+      target = ".config/hm-modules/gaming/gs.sh";
+      executable = true;
     };
+  };
 }
