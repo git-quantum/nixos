a36d02da40b3f1a11962aea861c40f56e4c13305
git-quantum <quentinhr@protonmail.com>
2024-09-29 04:41:20 +0200

    Update config file

--- nixos-flake/hosts/laptops/system76/configuration.nix
+++ nixos-flake/hosts/laptops/system76/configuration.nix
@@ -1,20 +1,17 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
-{
+{ config, pkgs, ... }: {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../../modules/nixos
     ../../../modules/core
-    
+
     ../../../modules/nixos/optional/desktop
+    ../../../modules/nixos/optional/gpu
+    ../../../modules/nixos/optional/mouse
     ../../../modules/nixos/optional/printing
     ../../../modules/nixos/optional/sound
-
-
+    ../../../modules/nixos/optional/ssd
+    ../../../modules/nixos/optional/system76
+    ../../../modules/nixos/optional/zram
   ];
 
   # Gaming mode
@@ -22,8 +19,6 @@
 
   # GPU
   custom.gpu.mode = "nvidia";
-
-
 
   users.users.quantum = {
     isNormalUser = true;
@@ -35,33 +30,6 @@
         #  thunderbird
       ];
   };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-   # Did you read the comment?
 
   # Disable password for Wheel usergroup
   security.sudo.wheelNeedsPassword = false;
