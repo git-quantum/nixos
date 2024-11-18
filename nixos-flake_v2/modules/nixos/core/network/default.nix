fc5574bccdb3292e70b7884f9c57aee18046cb60
git-quantum <quentinhr@pm.me>
2024-11-18 19:58:48 +0100

    Update

--- nixos-flake_v2/modules/nixos/core/network/default.nix
+++ nixos-flake_v2/modules/nixos/core/network/default.nix
@@ -1,6 +1,6 @@
-{
+{systemSettings, ...} : {
   networking = {
-    hostName = "laptop-system76";
+    hostName = systemSettings.hostname;
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     networkmanager.enable = true;
   };
